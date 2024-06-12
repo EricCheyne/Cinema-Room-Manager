6572982811cc125d01e7fe2ab6fbb204e59d4e8c
Eric Cheyne <eric.cheyne@gmail.com>
2024-06-11 22:08:46 -0700

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                Cinema:
+                  1 2 3 4 5 6 7 8
+                1 S S S S S S S S
+                2 S S S S S S S S
+                3 S S S S S S S S
+                4 S S S S S S S S
+                5 S S S S S S S S
+                6 S S S S S S S S
+                7 S S S S S S S S""");
+        }
+    }
---
+++ src/README.md
@@ -0,0 +1,5 @@
+# Cinema Room Manager
+
+There are many enjoyable activities on this funny little planet Earth, and still, the happiest, simplest, and most fulfilling one is probably going to the movies. Going with friends, going with loved ones, experiencing a whole new adventure from the safety of a cinema seat, surrounded by like-minded fellow travelers.
+
+Your good friends asked you to help them create an application for a cinema theatre where people can get tickets, reserve seats, and enjoy their movie night.
